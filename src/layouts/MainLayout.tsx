dcca07dc75f04d497572dfe0eca685507e5777bd
Ulil Amry <ulilamry432@gmail.com>
2025-02-17 18:55:22 +0700

    first commit

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/navbar'
+import React from 'react'
+
+const MainLayout = () => {
+  return (
+    <>
+    <Navbar/>
+    <div>MainLayout</div>
+    </>
+  )
+}
+
+export default MainLayout
