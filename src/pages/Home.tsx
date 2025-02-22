40252fc505b70f964dfba94aca333a36af44c069
Ulil Amry <ulilamry432@gmail.com>
2025-02-22 23:28:18 +0700

    feat:change title home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,21 +9,21 @@ const Home = () => {
     <MainLayout>
       <div className='relative'>
         <div>
-          <img className="h-auto md:h-[450px] max-w-full md:w-full object-cover rounded-lg" src="/images/banner.jpg" alt=""/>
+          <img className="h-auto md:h-[450px] max-w-full md:w-full object-cover rounded-lg" src="/images/banner.jpg" alt="" />
         </div>
         <div>
           <div className='absolute left-32 top-32 w-1/2 space-y-5'>
-              <div className='text-slate-50 text-5xl font-bold'>LOREM IPSUN DOLOR SIT AMET</div>
-              <div>
-                <p className='text-slate-50 font-semibold'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo nostrum reiciendis, perferendis repellat architecto nemo dolorem.
-                </p>
-                <Button variant="secondary">Button</Button>
-              </div>
+            <div className='text-slate-50 text-5xl font-bold'>LOREM IPSUN DOLOR SIT TIA SAYANGGG</div>
+            <div>
+              <p className='text-slate-50 font-semibold'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo nostrum reiciendis, perferendis repellat architecto nemo dolorem.
+              </p>
+              <Button variant="secondary">Button</Button>
             </div>
-            <div className='flex gap-4 justify-center -translate-y-14'>
-            <CardHeader/>
-            <CardHeader/>
-            <CardHeader/>
+          </div>
+          <div className='flex gap-4 justify-center -translate-y-14'>
+            <CardHeader />
+            <CardHeader />
+            <CardHeader />
 
           </div>
         </div>
